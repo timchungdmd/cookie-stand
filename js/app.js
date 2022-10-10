542b2e4263471dff972ee6800d89e14c40603898
timchungdmd <timchung.dmd@gmail.com>
2022-10-10 15:12:55 -0700

    salmon cookie sales data test

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const cookieStand=document.getElementById('salmon-cookies');
+let art=document.createElement('article');
+let ul=document.createElement('ul');
+art.textContent='Salmon Cookie Sales Data';
+art.appendChild(ul);
+
+let li=document.createElement('li');
+li.textContent='Seattle Salmon Cookie sales data'
+
+let sea_sale=document.createElement('article')
+sea_sales.textContent='Seattle Sale Data';
+art.appendChild(sea_sales);
